e0e1bfa7f419dcc78326fc08801ac9f8670668c6
Diones <pereiradiones987@gmail.com>
2025-01-25 18:03:29 -0300

    configurando variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
+      type: process.env.TYPEORM_CONNECTION as any || 'mariadb',
+      host: process.env.TYPEORM_HOST || 'localhost',
+      port: process.env.TYPEORM_PORT ? parseInt(process.env.TYPEORM_PORT, 10) : 3306,
+      username: process.env.TYPEORM_USERNAME || 'root',
       password: '',
-      database: 'nestjs_jwt_app',
+      database: process.env.TYPEORM_DATABASE || 'nestjs_jwt_app',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
-    })
+    } as TypeOrmModuleOptions),
   ],
   controllers: [],
   providers: [],
