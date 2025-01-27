8f60a5bb99547f2fa3a359b673a59647ea6fbb39
Diones <pereiradiones987@gmail.com>
2025-01-27 16:03:53 -0300

    criando hash da senha

--- src/app/users/user.entity.ts
+++ src/app/users/user.entity.ts
@@ -1,4 +1,12 @@
-import { Column, CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
+import { hashSync } from 'bcrypt';
 
 export class UsersEntity {
 
@@ -25,4 +33,9 @@ export class UsersEntity {
 
   @DeleteDateColumn({ name: 'deleted_at' })
   deletedAt: string;
+
+  @BeforeInsert()
+  hasPassword() {
+    return this.password = hashSync(this.password, 10);
+  }
 }
